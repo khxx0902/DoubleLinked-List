50a2339ba6816974724614f9e26fcc959e0f057d
TA1128 <m.khaidar.ft21@mail.umy.ac.id>
2022-11-16 16:36:37 +0700

    Node class represents the node of doubly linked list

--- DoubleLinked List/Program.cs
+++ DoubleLinked List/Program.cs
@@ -1,1 +1,12 @@
-﻿
+﻿using System;
+
+namespace DoublyLinkedList
+{
+    class Node
+    {
+        public int rollNumber;
+        public string name;
+        public Node next;
+        public Node prev;
+    }
+}
